24a44281f6f74a30ae8299f41be32e72a4689bd0
ThiagoMoraisAluno <thiagomoraiscordeiro@outlook.com>
2022-08-29 13:17:25 -0300

    models/user criação do banco de endereço

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
+from turtle import title
+
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    zipCode = models.IntegerField('Cep')
+    number = models.IntegerField('Número')
+    reference = models.CharField('Referência', max_length=100)
+    city = models.CharField('Cidade', max_length=50)
+    publicPlace = models.CharField('Logradouro', max_length=50)
+    district = models.CharField('Bairro', max_length=100)
+    state = models.CharField('Estado', max_length=30)
